5863ee993557b3a1d6b483bbb342fe6ebab5631e
Steve Rohrlack <elmo.dd@gmail.com>
2016-05-20 13:49:03 +0200

    added first draft of protocol for "Actors"

---
+++ src/Actors/Acting.swift
@@ -0,0 +1,13 @@
+//
+//  Acting.swift
+//  CitySimCore
+//
+//  Created by Steve Rohrlack on 20.05.16.
+//  Copyright © 2016 Steve Rohrlack. All rights reserved.
+//
+
+import Foundation
+
+protocol Acting {
+    mutating func act(on map: CityMap)
+}
